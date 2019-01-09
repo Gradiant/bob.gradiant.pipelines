9d1259ff6ef297af1f77b23ee4445706009a5fbe
acostapazo <costapazo@gmail.com>
2019-01-09 20:35:55 +0100

    Create ci.sh

---
+++ ci.sh
@@ -0,0 +1,20 @@
+
+function install_package(){
+    python bootstrap-buildout.py
+    bin/buildout
+}
+
+function test_package(){
+    bin/nosetests -v
+}
+
+function doc(){
+    bin/sphinx-build -b html doc doc/html
+    cd doc/html
+    echo "" >> .nojekyll
+    cd ../..
+}
+
+install_package
+test_package
+doc
